6447ec16b3d66c5033a953ee49bf14e43a8029f3
Isse Mohamed <99173622+issemoha@users.noreply.github.com>
2023-03-17 10:24:47 +0300

    Add files via upload

---
+++ TICTACTOE.py
@@ -0,0 +1,102 @@
+def printBoard(board):
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print("\n")
+
+
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if (checkDraw()):
+            print("Draw!")
+            exit()
+        if checkForWin():
+            if letter == 'X':
+                print(f"{player1} Wins!")
+                exit()
+            else:
+                print(f"{player2} wins!")
+                exit()
+
+        return
+
+
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        return True
+    else:
+        return False
+
+def checkDraw():
+    for key in board.keys():
+        if (board[key] == ' '):
+            return False
+    return True
+
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+
+
+def compMove():
+    position = int(input("Enter the position for 'X':  "))
+    insertLetter(bot, position)
+    return
+
+
+
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+
+
+printBoard(board)
+print("Computer goes first! Good luck.")
+print("Positions are as follow:")
+print("1, 2, 3 ")
+print("4, 5, 6 ")
+print("7, 8, 9 ")
+print("\n")
+player = 'O'
+bot = 'X'
+player1=str(input("Enter the name of Player 1: "))
+player2=str(input("Enter the name of Player 2: "))
+printBoard(board)
+
+while not checkForWin():
+    compMove()
+    playerMove()
